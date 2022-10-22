7adf0172238b528f90f8b9744d136d2f2f05c54f
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2022-10-22 15:40:12 +0300

    Update main.s

--- 5/main.s
+++ 5/main.s
@@ -42,18 +42,22 @@ Scan:
 	cmp	eax, 10000
 	jg	.L2
 	mov	eax, DWORD PTR n[rip]
-	# сравниваем n с 0;
+	# сравниваем c 0
 	test	eax, eax
 	jns	.L3
 .L2:
+	# Выводим надпись : "Введите n в промежутке от 0 до 10000"
 	lea	rax, .LC1[rip]
 	mov	rdi, rax
 	mov	eax, 0
 	call	printf@PLT
+	
+	# переходим на выход из функции если не удовлетворяет условиям 
+	jmp	.L4
 .L3:
 	mov	DWORD PTR -4[rbp], 0
-	jmp	.L4
-.L5:
+	jmp	.L5
+.L6:
 	# ввод чисел в массив A;
 	mov	eax, DWORD PTR -4[rbp]
 	cdqe
@@ -66,11 +70,14 @@ Scan:
 	mov	eax, 0
 	call	__isoc99_scanf@PLT
 	add	DWORD PTR -4[rbp], 1
-.L4:
+.L5:
+	# сравниваем n с 0
 	mov	eax, DWORD PTR n[rip]
 	cmp	DWORD PTR -4[rbp], eax
-	jl	.L5
+	jl	.L6
 	nop
+.L4:
+	# выход из функции Scan
 	nop
 	leave
 	ret
@@ -87,44 +94,41 @@ Comp:
 	mov	eax, DWORD PTR n[rip]
 	sub	eax, 1
 	mov	DWORD PTR -4[rbp], eax
-	jmp	.L7
-.L9:
+	jmp	.L8
+.L10:
 	mov	eax, DWORD PTR -4[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*4]
 	mov	rax, QWORD PTR -24[rbp]
 	add	rax, rdx
-	
-	
 	
 	# сравнение а[i] и 0;
 	mov	eax, DWORD PTR [rax]
 	test	eax, eax
-	jns	.L8
+	jns	.L9
 	mov	eax, DWORD PTR -4[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*4]
 	mov	rax, QWORD PTR -24[rbp]
 	add	rax, rdx
-	mov	edx, DWORD PTR x[rip]
+	mov	edx, DWORD PTR -8[rbp]
 	movsx	rdx, edx
 	lea	rcx, 0[0+rdx*4]
 	mov	rdx, QWORD PTR -32[rbp]
 	add	rdx, rcx
 	mov	eax, DWORD PTR [rax]
 	mov	DWORD PTR [rdx], eax
-	
 	# добавляем к у 1 (у += 1);
 	add	DWORD PTR -8[rbp], 1
-.L8:
+.L9:
 	sub	DWORD PTR -4[rbp], 1
-.L7:
+.L8:
 	cmp	DWORD PTR -4[rbp], 0
-	jns	.L9
+	jns	.L10
 	
 	# x = у;
 	mov	eax, DWORD PTR -8[rbp]
-	mov	DWORD PTR x[rip], eax 
+	mov	DWORD PTR x[rip], eax
 	nop
 	pop	rbp
 	ret
@@ -150,8 +154,8 @@ Prin:
 	mov	eax, 0
 	call	printf@PLT
 	mov	DWORD PTR -4[rbp], 0
-	jmp	.L11
-.L12:
+	jmp	.L12
+.L13:
 	mov	eax, DWORD PTR -4[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*4]
@@ -166,10 +170,10 @@ Prin:
 	mov	eax, 0
 	call	printf@PLT
 	add	DWORD PTR -4[rbp], 1
-.L11:
+.L12:
 	mov	eax, DWORD PTR x[rip]
 	cmp	DWORD PTR -4[rbp], eax
-	jl	.L12
+	jl	.L13
 	nop
 	nop
 	leave
@@ -200,17 +204,23 @@ main:
 	lea	rax, .LC4[rip]
 	mov	rsi, rax
 	mov	edi, 6
-	
 	# setlocate для пенчатанья текста на русском языке; 
 	call	setlocale@PLT
 	lea	rax, .LC5[rip]
 	mov	rdi, rax
 	mov	eax, 0
+	# выводим текст "Введите размер массива А, затем сам массив:"
 	call	printf@PLT
 	lea	rax, -80016[rbp]
 	mov	rdi, rax
 	# Вызываем функцию Scan;
 	call	Scan
+	mov	eax, DWORD PTR n[rip]
+	test	eax, eax
+	js	.L15
+	mov	eax, DWORD PTR n[rip]
+	cmp	eax, 10000
+	jg	.L15
 	lea	rdx, -40016[rbp]
 	lea	rax, -80016[rbp]
 	mov	rsi, rdx
@@ -221,12 +231,13 @@ main:
 	mov	rdi, rax
 	# Вызываем функцию Prin;
 	call	Prin
+.L15:
 	mov	eax, 0
 	mov	rdx, QWORD PTR -8[rbp]
 	sub	rdx, QWORD PTR fs:40
-	je	.L15
+	je	.L17
 	call	__stack_chk_fail@PLT
-.L15:
+.L17:
 	leave
 	ret
 	.size	main, .-main
